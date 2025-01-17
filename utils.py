d0eed2ebd46f3e62d5e616cbdd898dac4bb9e102
Hasitha Adikari <adikari.adikari@mycit.ie>
2025-01-17 10:56:30 +0000

    update auth token api

--- utils.py
+++ utils.py
@@ -1,13 +1,18 @@
 import requests
 
+AUTH_TOKEN_URL = 'http://localhost:8000/api/token-auth/'
+
 def get_token(username, password):
-    res = requests.post('http://localhost:8000/api/token-auth/',
+    res = requests.post(AUTH_TOKEN_URL,
                     data={'username': username,
                           'password': password}).json()
-    print(f"get_token|res: {res}")
-    token = res['token']
-    return token
+    if 'token' in res:
+        return res['token']
+    else:
+        print(f"get_token|token not found in res: {res}")
+        return None
 
 if __name__ == "__main__":
-    token = get_token("hasitha", "Hasitha@4805")
+    # token = get_token("hasitha", "Hasitha@4805")
+    token = get_token("hasitha", "hasdha.1")
     print(f"generated token : {token}")
