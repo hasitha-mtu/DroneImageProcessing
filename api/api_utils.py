5946a55832f9153504c6aa7c523b12fc441a77e9
Hasitha Adikari <adikari.adikari@mycit.ie>
2025-01-21 10:09:01 +0000

    log updates

--- api/api_utils.py
+++ api/api_utils.py
@@ -8,34 +8,34 @@ def request_url(api_section):
     return urljoin(BASE_URL, api_section)
 
 def get_request(url, token = None, headers = None, **kwargs):
-    print(f"get_request|url: {url}|headers: {headers}|kwargs: {kwargs}")
     if token:
         if headers:
             headers['Authorization'] = 'JWT {}'.format(token)
         else:
             headers = {'Authorization': 'JWT {}'.format(token)}
-    print(f"get_request|headers: {headers}")
     response = requests.get(url=url, headers= headers, **kwargs)
-    print(f"get_request|response: {response}")
     if response.status_code == 200:
         return response
     else:
+        print(f"get_request|url: {url}|headers: {headers}|kwargs: {kwargs}")
+        print(f"get_request|response: {response}")
         print("Failed to retrieve data", response.status_code)
         return None
 
 def post_request(url, token = None, headers = None, **kwargs):
-    print(f"post_request|url: {url}|headers: {headers}|kwargs: {kwargs}")
+
     if token:
         if headers:
             headers['Authorization'] = 'JWT {}'.format(token)
         else:
             headers = {'Authorization': 'JWT {}'.format(token)}
     response = requests.post(url=url, headers= headers, **kwargs)
-    print(f"post_request|response: {response}")
     if response.status_code == 200:
         return response.json()
     elif response.status_code == 201:
         return response.json()
     else:
+        print(f"post_request|url: {url}|headers: {headers}|kwargs: {kwargs}")
+        print(f"post_request|response: {response}")
         print("Failed to retrieve data", response.status_code)
         return None
